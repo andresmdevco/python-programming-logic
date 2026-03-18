1ea6880c8fbac1bed18e534a4487b0cc0cf25ca0
Andrés Muñoz <andres2078@gmail.com>
2026-03-18 15:42:22 -0500

    add sets and dictionary examples in 04_data_structures.py

--- 04_data_structures.py
+++ 04_data_structures.py
@@ -1,7 +1,12 @@
+"""
+Estructuras
+"""
+
 # Listas
 school_items = ["Cuaderno", "Lápiz", "Calculadora", "Tijeras"]
 print(school_items)
 school_items.append("Borrador") # Inserción
+school_items.append("Borrador")
 print(school_items)
 school_items.remove("Lápiz") # Eliminación
 print(school_items)
@@ -19,4 +24,34 @@ print(fruits)
 fruits = tuple(sorted(fruits)) # Ordenación
 print(fruits)
 print(type(fruits))
+
+# Sets: Estructura no ordenada - Datos no duplicados
+first_set = {"Gato", "Elefante", "León", "Canguro", "Cocodrilo"}
+print(first_set)
+first_set.add("andresu@gmail.com") # Inserción
+first_set.add("andresu@gmail.com")
+first_set.remove("Elefante")
+print(first_set)
+first_set = set(sorted(first_set)) # No se puede ordenar
+print(first_set)
+print(type(first_set))
+
+# Diccionario
+my_dict: dict = {
+    "name":"andres", 
+    "alias":"andresmdevco",
+    "age":"25", 
+    "nationality":"Colombiano", 
+}
+
+my_dict["email"] = "andres@gmail.com" # Inserción
+print(my_dict)
+del my_dict["nationality"] # Eliminación
+print(my_dict)
+print(my_dict["name"]) # Acceso
+my_dict["age"] = "35" # Actualización
+print(my_dict)
+my_dict = dict(sorted(my_dict.items())) # Ordenación
+print(my_dict)
+print(type(my_dict))
 
