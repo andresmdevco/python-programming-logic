7487512f5f0232a82dce9cc4f3813dd5c863184a
Andrés Muñoz <andres2078@gmail.com>
2026-03-25 22:23:24 -0500

    add classes with stack and queue structures

--- 09_classes.py
+++ 09_classes.py
@@ -15,9 +15,81 @@ class Programmer:
         print(f"Nombre: {self.name} | Apellidos: {self.surname} |Edad: {self.age} | Lenguajes {self.languages}")
 
 
-my_programmer = Programmer("Andrés", 25, ["C++", "Javascript", "Python"])
+my_programmer = Programmer("Andrés", 25, ["C++", "Javascript", "Python"]) # Instancia de la clase
 my_programmer.print()
 my_programmer.surname = "Muñoz"
 my_programmer.print()
 my_programmer.age = 26
 my_programmer.print()
+
+"""
+Extra
+
+Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas
+en el ejercicio número 7 de la ruta de estudio)
+- Deben poder inicializarse y disponer de operaciones para añadir, eliminar,
+retornar el número de elementos e imprimir todo su contenido.
+"""
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        return self.stack.append(item)
+
+    def pop(self):
+        if self.count() == 0:
+            return None
+        return self.stack.pop()
+
+    def count(self):
+        return len(self.stack)
+
+    def print(self):
+        for item in reversed(self.stack):
+            print(item)
+
+my_stack = Stack()
+my_stack.push(25)
+my_stack.push(35)
+my_stack.push(45)
+print(my_stack.count())
+my_stack.print()
+my_stack.pop()
+print(my_stack.count())
+my_stack.print()
+
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def push(self, item):
+        return self.queue.append(item)
+
+    def pop(self):
+        if self.count() == 0:
+            return None
+        return self.queue.pop(0)
+    
+    def count(self):
+        return len(self.queue)
+    
+    def print(self):
+        for item in self.queue:
+            print(item)
+
+
+my_queue = Queue()
+my_queue.push("Factura.pdf")
+my_queue.push("CV.pdf")
+my_queue.push("Inventario.pdf")
+print(my_queue.count())
+my_queue.print()
+my_queue.pop()
+print(my_queue.count())
+my_queue.print()
+
