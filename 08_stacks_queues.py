1100441a05aa804001e1e87353f7c52f73d1db95
Andrés Muñoz <andres2078@gmail.com>
2026-03-25 20:02:45 -0500

    add extra stacks and queues exercises

--- 08_stacks_queues.py
+++ 08_stacks_queues.py
@@ -41,3 +41,73 @@ print(queue.pop(0))
 
 print(queue)
 
+"""
+Extra
+
+- Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+el nombre de una nueva web.
+
+- Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+impresora compartida que recibe documentos y los imprime cuando así se le indica.
+La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+interpretan como nombres de documentos.
+"""
+
+# 1 - Web
+
+def web_nav():
+
+    stack = []
+
+    while True:
+
+        action = input(
+            "Añade una url o interactúa con palabras adelante/atrás/salir: "
+        )
+
+        if action == "salir":
+            print("Saliendo de la web.")
+            break
+        elif action == "adelante":
+            pass
+        elif action == "atrás":
+            if len(stack) > 0:
+                stack.pop()
+        else:
+            stack.append(action)
+
+        if len(stack) > 0:
+            print(f"Has navegado a la web: {stack[len(stack) - 1]}")
+        else:
+            print("Estas en la página de inicio.")
+
+# web_nav()
+
+
+
+# 1 - Impresora
+def shared_printed():
+    queue = []
+
+    while True:
+
+        action = input("Añada un documento o selecione imprimir/salir: ")
+        
+        if action == "salir":
+            print("Apagando la impresora.")
+            break
+        elif action == "imprimir":
+            if len(queue) > 0:
+                print(f"Imprimiendo el documento: {queue.pop(0)}")
+            else:
+                print("No hay ningun documento en la cola para imprimir")
+        else:
+            queue.append(action)
+
+        print(f"Cola de impresión: {queue}")
+
+
+shared_printed()
