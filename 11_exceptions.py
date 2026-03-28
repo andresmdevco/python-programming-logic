a7dbbaab973b71b3a3935bf99cffebee641991b0
Andrés Muñoz <andres2078@gmail.com>
2026-03-27 22:46:58 -0500

    add extra exceptions example

--- 11_exceptions.py
+++ 11_exceptions.py
@@ -6,5 +6,50 @@ try:
     print(10/1)
     print([1, 2, 3, 4][4])
 except Exception as e:
-    print(f"Se ha producido un error: {e}")
+    print(f"Se ha producido un error: {e} ({type(e).__name__})")
 
+
+"""
+Extra
+
+Crea una función que sea capaz de procesar parámetros, pero que también
+pueda lanzar 3 tipos diferentes de excepciones (una de ellas tiene que
+corresponderse con un tipo de excepción creada por nosotros de manera
+personalizada, y debe ser lanzada de manera manual) en caso de error.
+- Captura todas las excepciones desde el lugar donde llamas a la función.
+- Imprime el tipo de error.
+- Imprime si no se ha producido ningún error.
+- Imprime que la ejecución ha finalizado.
+"""
+
+class StrTypeError(Exception):
+    pass
+
+def process_params(parameters: list):
+    
+    if len(parameters) < 3:
+        raise IndexError()
+    elif parameters[1] == 0:
+        raise ZeroDivisionError()
+    elif type(parameters[2]) == str:
+        raise StrTypeError("El tercer elemento no puede ser una cadena de texto")
+
+    
+    print(parameters[2])
+    print(parameters[0]/parameters[1])
+    print(parameters[2] + 5)
+
+try:
+    process_params([1, 2, 3, 4])
+except IndexError as e:
+    print("El número de elementos de la lista debe ser mayor que dos.")
+except ZeroDivisionError as e:
+    print("El segundo elemento de la lista no puedo ser un cero.")
+except StrTypeError as e:
+    print(f"{e}")
+except Exception as e:
+    print(f"Se ha producido un error inesperado: {e}")
+else:
+    print("No se ha producido ningún error.")
+finally:
+    print("El programa finaliza sin detenerse")
