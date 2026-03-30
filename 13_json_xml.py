2e0f6e299062a8f85ac90086dd9072db6dfbd909
Andrés Muñoz <andres2078@gmail.com>
2026-03-29 22:18:30 -0500

    add extra json and xml exercise

--- 13_json_xml.py
+++ 13_json_xml.py
@@ -15,7 +15,7 @@ data = {
 # XML
 xml_file = "andres.xml"
 
-def save_xml():
+def create_xml():
 
     root = xml.Element("data")
 
@@ -30,7 +30,7 @@ def save_xml():
     tree = xml.ElementTree(root)
     tree.write(xml_file, encoding="utf-8")
 
-save_xml()
+create_xml()
 
 with open(xml_file, "r", encoding="utf-8") as xml_data:
     print(xml_data.read())
@@ -40,10 +40,63 @@ os.remove(xml_file)
 # JSON
 json_file = "andres.json"
 
-with open(json_file, "w", encoding="utf-8") as json_data:
-    json.dump(data, json_data, ensure_ascii=False)
+def create_json():
+    with open(json_file, "w", encoding="utf-8") as json_data:
+        json.dump(data, json_data, ensure_ascii=False)
+
+
+create_json()
 
 with open(json_file, "r", encoding="utf-8") as json_data:
     print(json_data.read())
 
+os.remove(json_file)
+
+"""
+Extra
+
+Utilizando la lógica de creación de los archivos anteriores, crea un
+programa capaz de leer y transformar en una misma clase custom de tu 
+lenguaje los datos almacenados en el XML y el JSON.
+Borra los archivos.
+"""
+
+create_xml()
+create_json()
+
+class Data:
+
+    def __init__(self, name, age, birth_date, programming_languages):
+        self.name = name
+        self.age = age
+        self.birth_date = birth_date
+        self.programming_languages = programming_languages
+
+
+with open(xml_file, "r", encoding="utf-8") as xml_data:    
+
+    root = xml.fromstring(xml_data.read())
+    name = root.find("name").text
+    age = root.find("age").text
+    birth_date = root.find("birth_date").text
+    programming_languages = []
+    for item in root.find("programming_languages"):
+        programming_languages.append(item.text)
+
+    xml_class = Data(name, age, birth_date, programming_languages)
+    print(xml_class.__dict__)
+
+
+with open(json_file, "r", encoding="utf-8") as json_data:   
+    json_dict = json.load(json_data)
+    json_class = Data(
+        json_dict["name"], 
+        json_dict["age"], 
+        json_dict["birth_date"], 
+        json_dict["programming_languages"]
+    )
+    print(json_class.__dict__)
+
+
+os.remove(xml_file)
 os.remove(json_file)
